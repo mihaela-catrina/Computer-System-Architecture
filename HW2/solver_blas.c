1993ffda85053d5d899e85153c5c1fb5bc9874a7
Catrina Mihela <catrina.mihaela20@gmail.com>
2019-04-11 12:47:53 +0300

    Add first version for solve_blast

--- HW2/solver_blas.c
+++ HW2/solver_blas.c
@@ -5,10 +5,32 @@
  */
 #include "utils.h"
 
-/* 
+/*
  * Add your BLAS implementation here
  */
 double* my_solver(int N, double *A, double *B) {
 	printf("BLAS SOLVER\n");
-	return NULL;
+	double alpha, beta;
+	// keep the result
+	double *C = calloc(N * N, sizeof(double));
+	alpha = 1.0;
+	beta = 0.0;
+	cblas_dgemm(CblasRowMajor, CblasTrans, CblasNoTrans,
+			N, N, N, alpha, B, N, A, N, beta, C, N);
+
+	alpha = 1.0;
+	beta = 1.0;
+	cblas_dgemm(CblasRowMajor, CblasTrans, CblasNoTrans,
+			N, N, N, alpha, A, N, B, N, beta, C, N);
+
+	double *D = calloc(N * N, sizeof(double));
+	alpha = 1.0;
+	beta = 0.0;
+	cblas_dgemm(CblasUpper, CblasNoTrans, CblasNoTrans,
+			N, N, N, alpha, C, N, C, N, beta, D, N);
+
+	mkl_free(C);
+
+	return D;
 }
+
