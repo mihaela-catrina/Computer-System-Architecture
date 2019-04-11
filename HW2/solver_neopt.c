08583b297b2223cf9bfb51906351e79886e8556b
Catrina Mihela <catrina.mihaela20@gmail.com>
2019-04-11 16:37:20 +0300

    Add first implementation for neopt solver

--- HW2/solver_neopt.c
+++ HW2/solver_neopt.c
@@ -9,6 +9,28 @@
  * Add your unoptimized implementation here
  */
 double* my_solver(int N, double *A, double* B) {
-	printf("NEOPT SOLVER\n");
-	return NULL;
+	double *C = calloc(N * N, sizeof(double));
+
+    for (size_t i = 0; i < N; i++) {
+        for (size_t j = 0; j < N; j++) {
+            if (i <= j) {
+                for (size_t k = 0; k < N; k++) {
+                    C[i*N+j] += A[k*N + i] * B[k*N + j] + B[j*N + k]*A[k*N + i];
+                }
+            }
+        }
+    }
+
+     int sum = 0;
+     for (size_t i = 0; i < N; i++) {
+     	for (size_t j = 0; j < N; j++) {
+     		for (size_t k = 0; k < N; k++) {
+     			sum += C[i*N + k] * C[k*N + j];
+     		}
+     		C[i*N + j] = sum;
+     		sum = 0;
+     	}
+     }
+
+	return C;
 }
